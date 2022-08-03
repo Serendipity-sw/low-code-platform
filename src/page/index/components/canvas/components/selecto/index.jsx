b3e13dfdeb2a20754f542e316fb7c79449c9cf07
gloomysw <sw.gloomysw@gmail.com>
2022-08-03 22:20:44 +0800

    :construction: 增加插入功能

--- src/page/index/components/canvas/components/selecto/index.jsx
+++ src/page/index/components/canvas/components/selecto/index.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
-import Selecto from "react-selecto";
+import Selecto from 'react-selecto'
+import { useDispatch, useSelector } from 'react-redux'
+import { AddPageItem } from '../../../../store/low-code-data'
+import { v4 as uuidV4 } from 'uuid'
 
-export default props=>{
+export default props => {
+  const insertControlsSelected = useSelector( state => state.lowCodeData.insertControlsSelected )
+
+  const dispatch = useDispatch()
+
+  const handleInsertControls = transformObj => {
+    if ( insertControlsSelected ) {
+      const canvasDomObj = document.querySelector( props.container ).getBoundingClientRect()
+      dispatch( AddPageItem( {
+        id: uuidV4(),
+        controls: insertControlsSelected,
+        style: {
+          transform: `translate(${ transformObj.left - canvasDomObj.left }px, ${ transformObj.top - canvasDomObj.top }px)`,
+          width: `${ transformObj.width }px`,
+          height: `${ transformObj.height }px`,
+          position: 'absolute'
+        }
+      } ) )
+    }
+  }
+
   return (
     <Selecto
-      container={props.container}
-      selectableTargets={[".target", document.querySelector(".target2")]}
-      selectByClick={true}
-      selectFromInside={true}
-      continueSelect={false}
-      toggleContinueSelect={"shift"}
-      keyContainer={props.container}
-      hitRate={100}
-      onSelectEnd={e => {
-        debugger
-
-      }}
+      dragContainer={ props.container }
+      selectByClick={ true }
+      selectFromInside={ true }
+      toggleContinueSelect={ 'shift' }
+      hitRate={ 100 }
+      ratio={ 0 }
+      onSelectEnd={ e => {
+        handleInsertControls( e.rect )
+      } }
     />
   )
 }
--- src/page/index/components/canvas/index.jsx
+++ src/page/index/components/canvas/index.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useRef, useState } from 'react'
 import style from './index.pcss'
 import MoveComponent from './components/move-component'
 import Selecto from './components/selecto'
+import { useSelector } from 'react-redux'
 
 export default _ => {
 
-  const domRef = useRef()
-  const dom1Ref = useRef()
+  const pageContentData = useSelector( state => state.lowCodeData.pageContentData )
+
+  const container = useRef()
+
   const [ visible, setVisible ] = useState( false )
 
   useEffect( _ => {
@@ -15,11 +18,16 @@ export default _ => {
 
   return (
     <>
-      <div ref={ dom1Ref } className={ style.init }>
-        <div ref={ domRef }>className</div>
-        <MoveComponent target={ domRef.current } container={ dom1Ref.current } viewAble={ true }/>
+      <div ref={ container } className={ style.init }>
+        {
+          pageContentData.map( item =>
+            <div id={ item.id } style={ item.style }>{ item.id }</div>
+          )
+        }
+        {/*{ pageContentData.length &&*/}
+        {/*  <MoveComponent target={ `#${ pageContentData[0].id }` } container={ `.${ style.init }` } viewAble={ true }/> }*/}
       </div>
-      <Selecto container={dom1Ref.current}/>
+      <Selecto container={ `.${ style.init }` }/>
     </>
   )
 }
