56f8f87508da653511e356280c733bb55c930a7e
serendipity <sw.gloomysw@gmail.com>
2022-08-03 11:37:47 +0800

    :construction: 文本插入

--- src/page/index/components/toolbar/components/insert-toolbar/index.jsx
+++ src/page/index/components/toolbar/components/insert-toolbar/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import style from './index.pcss'
+import InsertFont from './components/insert-font'
 
 export default props => {
   return (
     <div className={ [ style.init, props.className ].join( ' ' ) }>
+      <InsertFont/>
     </div>
   )
 }
