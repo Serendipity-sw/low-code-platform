56a19c6ffdf8cdbf943f7f21c4de8366fe0e7361
gloomysw <sw.gloomysw@gmail.com>
2022-08-05 23:10:42 +0800

    :construction: 文本删除线功能完成

--- src/page/index/components/toolbar/components/common-toolbar/components/del-line/index.jsx
+++ src/page/index/components/toolbar/components/common-toolbar/components/del-line/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import style from '../index.pcss'
 import IconFont from '../../../../../../../../components/icon-font'
+import { useDispatch, useSelector } from 'react-redux'
+import { cloneDeep } from 'lodash'
+import { EditPageItemList } from '../../../../../../store/lowCodeDataReducers'
 
 export default _ => {
+
+  const { selectControls, pageContentData } = useSelector( state => state.lowCodeData )
+
+  const dispatch = useDispatch()
+
+  const handleDelLine = _ => {
+    const list = cloneDeep( pageContentData.filter( item => selectControls.includes( item.id ) ) )
+    list.forEach( item => {
+      item.style.textDecoration = 'line-through'
+    } )
+    dispatch( EditPageItemList( list ) )
+  }
+
   return (
-    <div className={ style.column }>
+    <div className={ [ style.column, selectControls.length || style.disabled ].join( ' ' ) } onClick={ handleDelLine }>
       <IconFont className={ style.icon } name="#icon-shanchuxian"/>
       删除线
     </div>
