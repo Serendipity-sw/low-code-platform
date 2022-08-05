5dfb6c21e0334c29074ec32388c373fae479d4e0
gloomysw <sw.gloomysw@gmail.com>
2022-08-05 23:01:17 +0800

    :construction: 文字对齐方式完成

--- src/page/index/components/toolbar/components/common-toolbar/components/to-it/index.jsx
+++ src/page/index/components/toolbar/components/common-toolbar/components/to-it/index.jsx
@@ -2,26 +2,57 @@ import React from 'react'
 import style from '../index.pcss'
 import IconFont from '../../../../../../../../components/icon-font'
 import DropDown from '../../../../../../../../components/drop-down'
+import { useDispatch, useSelector } from 'react-redux'
+import { cloneDeep } from 'lodash'
+import { EditPageItemList } from '../../../../../../store/lowCodeDataReducers'
 
 export default _ => {
+
+  const cloneDeepProcessList = textAlign => {
+    const list = cloneDeep( pageContentData.filter( item => selectControls.includes( item.id ) ) )
+    list.forEach( item => {
+      item.style.textAlign = textAlign
+    } )
+    dispatch( EditPageItemList( list ) )
+  }
+
+  const handleAlignLeft = _ => {
+    cloneDeepProcessList( 'left' )
+  }
+
+  const handleAlignCenter = _ => {
+    cloneDeepProcessList( 'center' )
+  }
+
+  const handleAlignRight = _ => {
+    cloneDeepProcessList( 'right' )
+  }
+
   const dropDownList = [
     {
       iconName: 'icon-zuoduiqi',
-      name: '左对齐'
+      name: '左对齐',
+      onClick: handleAlignLeft
     },
     {
       iconName: 'icon-juzhongduiqi',
-      name: '居中对齐'
+      name: '居中对齐',
+      onClick: handleAlignCenter
     },
     {
       iconName: 'icon-youduiqi',
-      name: '右对齐'
+      name: '右对齐',
+      onClick: handleAlignRight
     }
   ]
 
+  const { selectControls, pageContentData } = useSelector( state => state.lowCodeData )
+
+  const dispatch = useDispatch()
+
   return (
-    <DropDown data={ dropDownList }>
-      <div className={ style.column }>
+    <DropDown data={ dropDownList } disabled={ selectControls.length === 0 }>
+      <div className={ [ style.column, selectControls.length || style.disabled ].join( ' ' ) }>
         <IconFont className={ style.icon } name="#icon-liangduanduiqi"/>
         排列
         <IconFont className={ style.dropDownIcon } name="#icon-xiala"/>
