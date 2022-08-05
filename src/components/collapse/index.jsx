01c26a19b9c11e79d951300c4d258418ab726780
serendipity <sw.gloomysw@gmail.com>
2022-08-05 17:32:00 +0800

    :construction: 完善折叠面板,增加header头部

--- src/components/collapse/index.jsx
+++ src/components/collapse/index.jsx
@@ -1,24 +1,48 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import style from './index.pcss'
-import { throttle } from 'lodash'
 
 export default props => {
 
   const collapseDom = useRef()
 
-  const handleCollapse = _ => {
-    switch ( props?.direction?.toLowerCase?.() ){
+  const [ collapseSize, setCollapseSize ] = useState( 0 )
+
+  useEffect( _ => {
+    if ( collapseSize === 0 ) {
+      setCollapseSize( collapseDom.current?.offsetWidth )
+    }
+    collapseDom.current?.addEventListener?.( 'transitionend', handleTransitionEnd )
+    return _ => collapseDom.current?.removeEventListener?.( 'transitionend', handleTransitionEnd )
+  }, [] )
+
+  useEffect( _ => {
+    if ( collapseSize ) {
+      switch ( props.direction?.toLowerCase?.() ) {
+        case 'width':
+          collapseDom.current.style.width = props.isOpened ? collapseSize : 0
+          break
+        case 'height':
+          collapseDom.current.style.height = props.isOpened ? collapseSize : 0
+          break
+      }
+    }
+  }, [ props.isOpened, collapseSize ] )
+
+  const handleTransitionEnd = _ => {
+    props?.onRest?.()
+  }
+
+  const directionClassProcess = direction => {
+    switch ( direction?.toLowerCase?.() ) {
       case 'width':
-        break
+        return style.collapseWidth
       case 'height':
-        break
-      default:
-        break
+        return style.collapseHeight
     }
   }
 
   return (
-    <div ref={collapseDom} className={ [ style.init, props.className ].join( ' ' ) } onClick={ throttle( handleCollapse, 200 ) }>
+    <div ref={ collapseDom } className={ [ directionClassProcess( props.direction ), props.className ].join( ' ' ) }>
       {
         props.children
       }
--- src/page/index/components/layer-area/index.jsx
+++ src/page/index/components/layer-area/index.jsx
@@ -1,17 +1,31 @@
 import React, { useState } from 'react'
 import style from './index.pcss'
+import Collapse from '../../../../components/collapse'
+import IconFont from '../../../../components/icon-font'
+import { throttle } from 'lodash'
 
-export default props => {
+export default _ => {
   const [ isOpened, setIsOpened ] = useState( true )
 
   const clickOpened = _ => {
-    debugger
     setIsOpened( !isOpened )
   }
 
-  return (
-    <div className={ style.init } onClick={ clickOpened }>
+  const collapseWorkCallback = _ => {
+    debugger
+  }
 
-    </div>
+  return (
+    <Collapse className={ style.init } direction="width" isOpened={ isOpened } onRest={ collapseWorkCallback }>
+      <div className={ style.init }>
+        <header className={ style.header }>
+          <span className={ style.title }>
+            <IconFont className={ style.icon } name="#icon-zengjiatuceng" onClick={ throttle( clickOpened, 200 ) }/>
+            图层管理
+          </span>
+          <IconFont className={ style.hideIcon } name="#icon-zhedie1" onClick={ throttle( clickOpened, 200 ) }/>
+        </header>
+      </div>
+    </Collapse>
   )
 }
