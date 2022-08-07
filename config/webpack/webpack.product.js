d7fd0149c0a081fabfc64cb312e4e9bd54193ce4
gloomysw <sw.gloomysw@gmail.com>
2022-08-07 22:59:59 +0800

    :construction: 修正配置,更正版本号,调整项目名称

--- config/webpack/webpack.product.js
+++ config/webpack/webpack.product.js
@@ -6,7 +6,7 @@ let config = merge(baseWebpackConfig, {
   plugins: [
     new webpack.DefinePlugin({
       Gloomy_env: JSON.stringify('production'),
-      Map_Key: JSON.stringify('')
+      Map_Key: JSON.stringify('b6f3a086b83159ee8f080b361d1384d2')
     })
   ]
 })
