d7e7f9ecc2ee7595d2bbc04ddaaf004786f26322
gloomysw <sw.gloomysw@gmail.com>
2022-08-08 22:21:35 +0800

    :construction: 增加样式组件

--- src/page/index/components/panel/index.jsx
+++ src/page/index/components/panel/index.jsx
@@ -1,34 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './index.pcss'
 import { Drawer, Segmented } from 'antd'
 import Style from './components/style'
+import { menuTypeMap } from './store/menu-type'
 
 export default _ => {
+
+  const [ selectTab, setSelectTab ] = useState( menuTypeMap.style.value )
+
   return (
     <Drawer
       title="操作面板"
       placement="right"
       closable={ false }
-      visible={ false }
+      visible={ true }
       mask={ false }
       getContainer={ false }
       className={ style.init }
     >
       <div className={ style.segmented }>
-        <Segmented options={ [
-          {
-            label: '样式',
-            value: 1
-          },
-          {
-            label: '数据',
-            value: 2
-          },
-          {
-            label: '事件',
-            value: 3
-          }
-        ] }/>
+        <Segmented value={ selectTab } onChange={ value => setSelectTab( value ) }
+                   options={ Object.entries( menuTypeMap ).map( item => item[1] ) }/>
+        <Style className={ menuTypeMap.style.value === selectTab || style.none }/>
       </div>
       <Style/>
     </Drawer>
