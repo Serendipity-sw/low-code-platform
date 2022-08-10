3717c209d7b235c14facbeb6e5e64cada5643677
gloomysw <sw.gloomysw@gmail.com>
2022-08-10 21:50:10 +0800

    :construction: 开启mock能力

--- config/webpack/webpack.product.js
+++ config/webpack/webpack.product.js
@@ -6,7 +6,7 @@ let config = merge(baseWebpackConfig, {
   plugins: [
     new webpack.DefinePlugin({
       Gloomy_env: JSON.stringify('production'),
-      Enable_Mock: JSON.stringify( false ),
+      Enable_Mock: JSON.stringify( true ),
       Map_Key: JSON.stringify('b6f3a086b83159ee8f080b361d1384d2')
     })
   ]
