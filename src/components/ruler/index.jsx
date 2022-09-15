f41f12772aec1e052c4d840e934a2a32eb32ecac
serendipity <sw.gloomysw@gmail.com>
2022-09-15 15:42:29 +0800

    :hammer: 增加标尺能力

--- src/components/ruler/index.jsx
+++ src/components/ruler/index.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import style from './index.pcss'
+import Guides from '@scena/guides'
 
 export default props => {
-  return <div className={ style.init }>
-    <div className={ style.horizontal }></div>
-    <div className={ style.vertical }></div>
-    {
-      props.children
+  const parentDom = useRef(), horizontalDom = useRef(), verticalDom = useRef()
+
+  useEffect( _ => {
+    const horizontalGuides = new Guides( horizontalDom.current, {
+        type: 'horizontal',
+        displayDragPos: true
+      } ),
+      verticalGuides = new Guides( verticalDom.current, {
+        type: 'vertical',
+        displayDragPos: true
+      } )
+
+    const parentDomObserver = new ResizeObserver( _ => {
+      horizontalGuides.resize()
+      verticalGuides.resize()
+    } )
+
+    parentDomObserver.observe( parentDom.current )
+
+    return _ => {
+      parentDomObserver.disconnect()
+      horizontalGuides.destroy()
+      verticalGuides.destroy()
     }
-  </div>
+  }, [] )
+
+  return (
+    <div ref={ parentDom } className={ [ style.init, props.className ].join( ' ' ) }>
+      <i className={ style.box } />
+      <div ref={ horizontalDom } className={ style.horizontal }></div>
+      <div ref={ verticalDom } className={ style.vertical }></div>
+      <div className={ style.content }>
+        { props.children }
+      </div>
+    </div>
+  )
 }
