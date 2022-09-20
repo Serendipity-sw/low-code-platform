058d6ddfb2e2bcf31739be05f4085c143def5dce
serendipity <sw.gloomysw@gmail.com>
2022-09-20 15:02:17 +0800

    :wrench: 优化webpack构建性能

--- config/webpack/webpack.conf.js
+++ config/webpack/webpack.conf.js
@@ -1,104 +1,89 @@
-const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' )
-const CssMinimizerPlugin = require( 'css-minimizer-webpack-plugin' )
-const ParallelUglifyPlugin = require( 'webpack-parallel-uglify-plugin' )
-const path = require( 'path' )
-const Webpack = require( 'webpack' )
-const WebpackBar = require( 'webpackbar' )
-const CompressionWebpackPlugin = require( 'compression-webpack-plugin' )
-const TerserPlugin = require( 'terser-webpack-plugin' )
-const portFinderSync = require( 'portfinder-sync' )
-const DashboardPlugin = require( 'webpack-dashboard/plugin' )
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
+const path = require('path')
+const WebpackBar = require('webpackbar')
+const TerserPlugin = require('terser-webpack-plugin')
+const portFinderSync = require('portfinder-sync')
 
 module.exports = {
-  entry: './src/app.jsx',
-  output: {
-    charset: true,
-    path: path.resolve( __dirname, '../../dist' ),
-    filename: './js/[name].bundle.[chunkhash].js',
-    clean: true
-  },
-  resolve: {
-    alias: {
-      src: path.resolve( __dirname, '../../src/' )
+    entry: './src/app.jsx',
+    output: {
+        charset: true,
+        path: path.resolve(__dirname, '../../dist'),
+        filename: './js/[name].bundle.[chunkhash].js',
+        clean: true
     },
-    extensions: [ '.js', '.jsx', '.json' ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.(?:ico|png|svg|jpg|jpeg|gif)$/i,
-        loader: 'url-loader',
-        options: {
-          name: './static/[name].[chunkhash].[ext]'
+    resolve: {
+        alias: {
+            src: path.resolve(__dirname, '../../src/')
+        },
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: [
+                    'cache-loader',
+                    'thread-loader',
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(?:ico|png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 25 * 1024, // 25kb
+                    }
+                },
+                generator: {
+                    // 打包到 image 文件下
+                    filename: './images/[contenthash][ext][query]',
+                }
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 25 * 1024, // 25kb
+                    }
+                },
+                generator: {
+                    // 打包到 image 文件下
+                    filename: './images/[contenthash][ext][query]',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new WebpackBar({}),
+        new MiniCssExtractPlugin({
+            filename: './css/[name].bundle.[chunkhash].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: '低代码平台',
+            template: path.resolve(__dirname, '../../template.html'),
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    devServer: {
+        host: '127.0.0.1',
+        port: portFinderSync.getPort(3000),
+        hot: true,
+        open: true,
+        client: {
+            overlay: true,
+            progress: true
         }
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        loader: 'url-loader',
-        options: {
-          name: './static/[name].[chunkhash].[ext]'
-        }
-      }
-    ]
-  },
-  optimization: {
-    usedExports: true,
-    minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-      new CssMinimizerPlugin( ),
-      new ParallelUglifyPlugin( {
-        cacheDir: '.cache/',
-        test: /.js$/,
-        workerCount: 2,
-        uglifyJS: {
-          output: {
-            beautify: false,
-            comments: false
-          },
-          compress: {
-            drop_console: true,
-            collapse_vars: true,
-            reduce_vars: true
-          }
-        }
-      } )
-    ]
-  },
-  plugins: [
-    new WebpackBar( {} ),
-    new MiniCssExtractPlugin( {
-      filename: './css/[name].bundle.[chunkhash].css'
-    } ),
-    new HtmlWebpackPlugin( {
-      title: '低代码平台',
-      template: path.resolve( __dirname, '../../template.html' ),
-      filename: 'index.html',
-      inject: 'body'
-    } ),
-    new DashboardPlugin(),
-    new CompressionWebpackPlugin( {
-      algorithm: 'gzip'
-    } )
-  ],
-  devServer: {
-    host: '127.0.0.1',
-    port: portFinderSync.getPort( 3000 ),
-    hot: true,
-    open: true,
-    client: {
-      overlay: true,
-      progress: true
     }
-  }
 }
--- config/webpack/webpack.product.js
+++ config/webpack/webpack.product.js
@@ -2,6 +2,10 @@ const {merge} = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.conf')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' )
+const DashboardPlugin = require("webpack-dashboard/plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ParallelUglifyPlugin = require("webpack-parallel-uglify-plugin");
 
 let config = merge(baseWebpackConfig, {
   module:{
@@ -36,7 +40,34 @@ let config = merge(baseWebpackConfig, {
   performance: {
     hints: false,
   },
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new ParallelUglifyPlugin({
+        cacheDir: '.cache/',
+        test: /.js$/,
+        workerCount: 2,
+        uglifyJS: {
+          output: {
+            beautify: false,
+            comments: false
+          },
+          compress: {
+            drop_console: true,
+            collapse_vars: true,
+            reduce_vars: true
+          }
+        }
+      })
+    ]
+  },
   plugins: [
+    new DashboardPlugin(),
+    new CompressionWebpackPlugin({
+      algorithm: 'gzip'
+    }),
     new webpack.DefinePlugin({
       Gloomy_env: JSON.stringify('production'),
       Enable_Mock: JSON.stringify( true ),
