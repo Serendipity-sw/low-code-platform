05957dbde1596e61ef01181be6cf134edfa6473f
serendipity <sw.gloomysw@gmail.com>
2022-09-22 14:42:51 +0800

    :construction: 改变组件形态

--- src/page/index/index.jsx
+++ src/page/index/index.jsx
@@ -6,21 +6,16 @@ import Canvas from './components/canvas'
 import Panel from './components/panel'
 import LayerArea from './components/layer-area'
 
-class Index extends React.Component {
-
-  render() {
-    return (
-      <div className={ style.init }>
-        <Header/>
-        <Toolbar/>
-        <div className={ style.editArea }>
-          <LayerArea/>
-          <Canvas/>
-          <Panel/>
-        </div>
+export default _ => {
+  return (
+    <div className={style.init}>
+      <Header/>
+      <Toolbar/>
+      <div className={style.editArea}>
+        <LayerArea/>
+        <Canvas/>
+        <Panel/>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default Index
