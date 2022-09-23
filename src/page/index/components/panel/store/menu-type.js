6fb18c707a019267e0d84f47d144ca8db34edc41
serendipity <sw.gloomysw@gmail.com>
2022-09-23 14:43:33 +0800

    :construction: 全组件支持样式 数据 事件能力

--- src/page/index/components/panel/store/menu-type.js
+++ src/page/index/components/panel/store/menu-type.js
@@ -1,4 +1,4 @@
-import { controlsType } from '../../../utils/controls-type'
+import {controlsType} from '../../../utils/controls-type'
 
 export const menuTypeMap = {
   [controlsType.div.name]: {
@@ -44,53 +44,73 @@ export const menuTypeMap = {
     }
   },
   [controlsType.map.name]: {
+    style: {
+      label: '样式',
+      value: 1
+    },
     data: {
       label: '数据',
-      value: 1
+      value: 2
     },
     event: {
       label: '事件',
-      value: 2
+      value: 3
     }
   },
   [controlsType.chartLine.name]: {
+    style: {
+      label: '样式',
+      value: 1
+    },
     data: {
       label: '数据',
-      value: 1
+      value: 2
     },
     event: {
       label: '事件',
-      value: 2
+      value: 3
     }
   },
   [controlsType.chartColumn.name]: {
+    style: {
+      label: '样式',
+      value: 1
+    },
     data: {
       label: '数据',
-      value: 1
+      value: 2
     },
     event: {
       label: '事件',
-      value: 2
+      value: 3
     }
   },
   [controlsType.chartBar.name]: {
+    style: {
+      label: '样式',
+      value: 1
+    },
     data: {
       label: '数据',
-      value: 1
+      value: 2
     },
     event: {
       label: '事件',
-      value: 2
+      value: 3
     }
   },
   [controlsType.chartPie.name]: {
+    style: {
+      label: '样式',
+      value: 1
+    },
     data: {
       label: '数据',
-      value: 1
+      value: 2
     },
     event: {
       label: '事件',
-      value: 2
+      value: 3
     }
   }
 }
